22746a445fe0a54d4b83fe48b9a502d7c2d9e61c
Ruby <122392242+YidanWWW@users.noreply.github.com>
2024-10-14 16:41:30 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ data "aws_availability_zones" "available" {
 module "networking" {
   source             = "./networking"
   project            = var.project
-  #region             = var.region
+  region             = var.region
   vpc_cidr           = var.vpc_cidr
   availability_zones = data.aws_availability_zones.available.names
   unique_suffix      = random_pet.unique_suffix.id
